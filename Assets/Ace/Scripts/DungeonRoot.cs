c224980d0ebf1ee2c764058a7c5ebfb46921ed05
Ace English <Ace English>
2022-01-06 23:05:39 -0800

    next room indicator

---
+++ Assets/Ace/Scripts/DungeonRoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonRoot : MonoBehaviour
+{
+
+
+    #region Singleton
+    private static DungeonRoot _instance;
+    public static DungeonRoot Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<DungeonRoot>();
+                if (_instance == null)
+                {
+                    print("Creating new DungeonRoot!");
+                    _instance = new GameObject().AddComponent<DungeonRoot>();
+                }
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ace/Scripts/NextRoomIndicator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextRoomIndicator : MonoBehaviour
+{
+    public GameObject arrowIndicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseEnter()
+    {
+        arrowIndicator.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        arrowIndicator.SetActive(false);
+    }
+    void OnMouseDown()
+    {
+        print("Pribyet!");
+    }
+}
